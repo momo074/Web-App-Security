fdcfb5e8e10a9e9a7e6b7bb803f8e42f4cd0ca73
momo074 <167190095+momo074@users.noreply.github.com>
2024-05-24 14:58:58 +0200

    Update app_security.py

--- app_security.py
+++ app_security.py
@@ -1,6 +1,13 @@
 import os
 from flask import Flask, request, redirect, url_for
 
+def ip_analyzer(ip_address):
+    try:
+        host_name = socket.gethostbyaddr(ip_address)
+        print(f"L'adresse IP {ip_address} correspond à l'hôte {host_name[0]}")
+    except socket.herror:
+        print(f"Impossible de trouver l'hôte correspondant à l'adresse IP {ip_address}")
+        
 app = Flask(__name__)
 
 @app.route('/')
